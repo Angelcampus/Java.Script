c0d43868906415e27f3df1da6b24eb4c68bf78cd
Angelcampus <angel.visticot@le-campus-numerique.fr>
2025-04-24 16:45:24 +0200

    exo 5 fini

---
+++ exo-5.js
@@ -0,0 +1,15 @@
+let input= "maemuv modmd spqf eire";
+let vowels =["a","e","y","u","i","o",]
+let cherche= input.indexOf(vowels)
+let resultArray = [];
+for (i=0;i<input.length;i++){
+    for (y=0;y<vowels.length;y++)
+    //console.log(input[i])
+    if (input[i]==vowels[y]){
+    console.log(input[i],i);
+    resultArray.push(input[i]);
+    }
+}
+resultArray=resultArray.join("");
+resultArray=resultArray.toUpperCase();
+console.log(resultArray);
