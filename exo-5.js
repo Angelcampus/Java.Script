8bd2aff3927fd20b28e54e5876f90741d887e4b1
Angelcampus <angel.visticot@le-campus-numerique.fr>
2025-04-25 16:15:46 +0200

    super modif qui sert à rien

--- exo-5.js
+++ exo-5.js
@@ -1,6 +1,6 @@
 let input= "maemuv modmd spqf eire";
 let vowels =["a","e","y","u","i","o",]
-let cherche= input.indexOf(vowels)
+//let cherche= input.indexOf(vowels)
 let resultArray = [];
 for (i=0;i<input.length;i++){
     for (y=0;y<vowels.length;y++)
